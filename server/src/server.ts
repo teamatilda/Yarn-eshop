2cf2b997d2e08fef40d4048a7f36b2c6533b5e91
Tea Westling <teawest99@gmail.com>
2026-09-16 16:12:05 +0200

    added images

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,23 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import productRouter from "./routes/products.js";
 
 // Skapa express-applikationen
 const app = express();
 
+
+// Behövs för __dirname i ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log("Serving images from:", path.join(__dirname, "public/images"));
+
 // Använd port 8000
 const port = process.env.PORT || 8000;
+
+// Servera statiska bilder från public/images
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 // Alla HTTP-anrop
 // "/api/products" ska hanteras av issueRouter
