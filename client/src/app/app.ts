4b53b15a16ebdb25d151ac8252c6a833d99662c6
Tea Westling <teawest99@gmail.com>
2026-09-16 11:54:35 +0200

    footer html and css

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -3,13 +3,15 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { Home  } from './routes/home/home'; 
 import { FrontpageProducts } from './routes/frontpage-products/frontpage-products';
+import { Footer } from './routes/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Home, FrontpageProducts, RouterLink, RouterOutlet],
+  imports: [Home, FrontpageProducts, Footer, RouterLink, RouterOutlet],
   template: `
   <app-home />
-  <app-frontpage-products />`,
+  <app-frontpage-products />
+  <app-footer />`,
   styleUrl: './routes/home/home.css',
 })
 export class App {
