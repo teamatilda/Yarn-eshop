72af61e45f35829b0981371373ed4b97e0ecdfbb
Tea Westling <teawest99@gmail.com>
2026-09-19 12:22:25 +0200

    created search-result component

---
+++ client/src/app/routes/search-result/search-result.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-search-result',
+  styleUrl: './search-result.css',
+  templateUrl: './search-result.html',
+})
+export class SearchResult {}
