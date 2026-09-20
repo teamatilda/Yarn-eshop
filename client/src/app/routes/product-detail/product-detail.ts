dc8e41142c20b084d8415b7117ee54ef15bddc3b
Tea Westling <teawest99@gmail.com>
2026-09-20 11:25:43 +0200

    created product-detail component

---
+++ client/src/app/routes/product-detail/product-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-product-detail',
+  styleUrl: './product-detail.css',
+  templateUrl: './product-detail.html',
+})
+export class ProductDetail {}
