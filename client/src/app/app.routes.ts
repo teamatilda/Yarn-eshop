9815c55df45a9459894939ea903856f011234696
Tea Westling <teawest99@gmail.com>
2026-09-21 10:24:36 +0200

    created table for admin page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './routes/home/home';
 import { SearchResult } from './routes/search-result/search-result';
 import { ProductDetail } from './routes/product-detail/product-detail';
+import { AdminPage } from './routes/admin-page/admin-page';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'search', component: SearchResult },
-    { path: 'product/:slug', component: ProductDetail}
+    { path: 'product/:slug', component: ProductDetail },
+    { path: 'admin/products', component: AdminPage },
 ];
---
+++ client/src/app/main-layout/main-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-main-layout',
+  styleUrl: './main-layout.css',
+  templateUrl: './main-layout.html',
+})
+export class MainLayout {}
